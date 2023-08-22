ba95be22256100e30bd0c4f328f6eac6c7c688bd
Praveen Prasad <praveenprasad2510@gmail.com>
2023-08-22 20:42:59 +0530

    Completed user authentication and front end design of landing page

---
+++ Client/src/Api/Api.jsx
@@ -0,0 +1,2 @@
+export const signUp="http://localhost:8080/signup"
+export const signIn="http://localhost:8080/signin"
--- Client/src/Components/LandingPage/LandingPage.jsx
+++ Client/src/Components/LandingPage/LandingPage.jsx
@@ -1,17 +1,16 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import "./landing.scss";
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import { signIn, signUp } from "../../Api/Api";
+import axios from "axios";
 
 function LandingPage() {
-
-
+  // Signup Dialog
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -21,6 +20,65 @@ function LandingPage() {
   const handleClose = () => {
     setOpen(false);
   };
+
+  // Signup function
+
+  const usernameRef = useRef();
+  const emailRef = useRef();
+  const phonenoRef = useRef();
+  const passwordRef = useRef();
+
+  async function doSignup() {
+    if (
+      usernameRef.current.value &&
+      emailRef.current.value &&
+      phonenoRef.current.value &&
+      passwordRef.current.value
+    ) {
+      let newUser = {
+        username: usernameRef.current.value,
+        email: emailRef.current.value,
+        phoneno: phonenoRef.current.value,
+        password: passwordRef.current.value,
+      };
+      let response = await axios.post(signUp, newUser);
+      setOpen(false);
+      setOpenSignin(true);
+    } else {
+      alert("error");
+    }
+  }
+
+  // Signin Dialog
+
+  const [openSignin, setOpenSignin] = React.useState(false);
+
+  const handleClickOpenSignin = () => {
+    setOpenSignin(true);
+  };
+
+  const handlecloseSignin = () => {
+    setOpenSignin(false);
+  };
+
+  //Signin function
+
+  const userRef = useRef();
+  const passRef = useRef();
+
+  async function doSignin() {
+    if (userRef.current.value && passRef.current.value) {
+      let user = {
+        username: userRef.current.value,
+        password: passRef.current.value,
+      };
+      let responsesign = await axios.post(signIn, user);
+      alert("Success");
+      setOpenSignin(false);
+    } else {
+      alert("sorry");
+    }
+  }
 
   return (
     <>
@@ -38,8 +96,12 @@ function LandingPage() {
                 connections."
               </p>
               <div className="buttons">
-                <button className="btn">Signin</button>
-                <button className="btn" onClick={handleClickOpen}>Signup</button>
+                <button className="btn" onClick={handleClickOpenSignin}>
+                  Signin
+                </button>
+                <button className="btn" onClick={handleClickOpen}>
+                  Signup
+                </button>
               </div>
             </div>
           </div>
@@ -47,48 +109,90 @@ function LandingPage() {
             <video autoPlay muted loop className="video-bg">
               <source src="\src\assets\bg-video.mov" type="video/webm" />
             </video>
-
           </div>
         </div>
 
-{/* Signup Dialog  */}
-            <div >
-      
-      <Dialog open={open} onClose={handleClose} >
+        {/* Signup Dialog  */}
+        <div>
+          <Dialog open={open} onClose={handleClose}>
+            <h1>Signup</h1>
+            <DialogContent className="signup-dialog">
+              <TextField
+                autoFocus
+                margin="dense"
+                id="username"
+                label="Username"
+                type="string"
+                variant="standard"
+                inputRef={usernameRef}
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                id="email"
+                label="Email Address"
+                type="email"
+                variant="standard"
+                inputRef={emailRef}
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                id="phone"
+                label="Phone No"
+                type="string"
+                variant="standard"
+                inputRef={phonenoRef}
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                id="password"
+                label="Password"
+                type="password"
+                variant="standard"
+                inputRef={passwordRef}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>Cancel</Button>
+              <Button onClick={doSignup}>Signup</Button>
+            </DialogActions>
+          </Dialog>
+        </div>
 
-        <h1>Signup</h1>
-        <DialogContent className="signup-dialog">
-          <TextField
-            autoFocus
-            margin="dense"
-            id="username"
-            label="Username"
-            type="string"
-            variant="standard"
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="email"
-            label="Email Address"
-            type="email"
-            variant="standard"
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="password"
-            label="Password"
-            type="password"
-            variant="standard"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Signup</Button>
-        </DialogActions>
-      </Dialog>
-    </div>
+        {/* Signin Dialog  */}
+
+        <div>
+          <Dialog open={openSignin} onClose={handlecloseSignin}>
+            <h1>Signup</h1>
+            <DialogContent className="signup-dialog">
+              <TextField
+                autoFocus
+                margin="dense"
+                id="username"
+                label="Username"
+                type="string"
+                variant="standard"
+                inputRef={userRef}
+              />
+
+              <TextField
+                autoFocus
+                margin="dense"
+                id="password"
+                label="Password"
+                type="password"
+                variant="standard"
+                inputRef={passRef}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handlecloseSignin}>Cancel</Button>
+              <Button onClick={doSignin}>Signin</Button>
+            </DialogActions>
+          </Dialog>
+        </div>
       </div>
     </>
   );
